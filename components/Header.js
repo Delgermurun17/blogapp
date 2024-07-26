33a1027d0f4968259a50254cc9a24a0e462b0017
Delgermurun <88150265+Delgermurun17@users.noreply.github.com>
2024-07-26 17:21:02 +0800

    deploy

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,21 @@
 import Image from "next/image";
 import { NavItem } from "./NavItem";
+import Link from "next/link";
 
 export function Header(){
     return (
         <div className="flex justify-between items-center">
             <div>
+                <Link target="_self" href="/">
                 <div className="hidden md:block"><Image width={158} height={36} src={"logo.svg"}/></div>
                 <div className="md:hidden"><Image width={135} height={26} src={"logo.svg"}/></div>
+                </Link>
             </div>
-            <div className="flex">
+            <div className="flex space-x-3">
+            <Link target="_self" href="/">
                 <NavItem nav="Home"/>
-                <NavItem nav="Blog"/>
+            </Link>
+                <Link href={"blog"}> <NavItem nav="Blog"/></Link>
                 <NavItem nav="Contact"/>
             </div>
             <div className="">
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { Header } from "@/components/Header";
+import { Main } from "@/components/Main";
 
 export default function Home() {
   return (
-    <main>
+    <main className="dark:bg-gray-800 bg-gray-100">
       <Header />
+      <Main />
     </main>
   );
 }
