64962b6bd5bc5af966a0388297591e89cd41ec9c
Delgermurun <88150265+Delgermurun17@users.noreply.github.com>
2024-08-02 16:42:03 +0800

    deploy

--- components/Footer.js
+++ components/Footer.js
@@ -8,7 +8,34 @@ import Image from "next/image";
 import "@fontsource/plus-jakarta-sans"
 
 export function Footer(){
-    return (
+    return (<div>
+        <div className="md:hidden">
+            <div className="border border-[#f7f7f7] w-[240px] mx-auto"></div>
+        <div className="h-[289px] p-[30px_0px] flex flex-col items-center gap-[30px]">
+            <div className="h-[88px] flex justify-center">
+                        <div className="flex flex-col gap-2">
+                        <Link target="_self" href="/"><Footeritem fItem="Home"/></Link>
+                        <Link href={"blog"}><Footeritem fItem="Blog"/></Link>
+                        <Link href={"contact"}><Footeritem fItem="Contact"/></Link>
+                        </div>
+            </div>
+            <div className="w-36 flex justify-between text-[#6D6E76]">
+                        <Link target="_blank" href={"https://facebook.com"}><FaFacebook /></Link>
+                        <Link target="_blank" href={"https://twitter.com"}><FaTwitter />  </Link>
+                        <Link target="_blank" href={"https://instagram.com"}><FaInstagram /></Link>
+                        <Link target="_blank" href={"https://linkedin.com"}><FaLinkedin /></Link>
+            </div>
+            <div className="flex items-center justify-between gap-5 h-24">
+                    <div className="flex gap-2.5 items-center">
+                        <div><Image src={"/footericon.svg"} width={48} height={48}/></div>
+                        <div>
+                            <div className="font-normal text-xl text-[#141624]">Meta<span className="font-extrabold">Blog</span></div>
+                            <Footeritem fItem="© All Rights Reserved."/>
+                        </div>
+                    </div>
+                </div>
+        </div></div>
+        <div className="hidden md:block">
         <div className="h-[490px] border border-t-[#E8E8EA] bg-[#F6F6F7] flex items-center font-jakarta">
             <div className="md:w-[1216px] w-full mx-auto flex flex-col gap-[25px]">
                 <div className="flex gap-5 h-60">
@@ -53,6 +80,8 @@ export function Footer(){
                     </div>
                 </div>
             </div>
+        </div>
+        </div>
         </div>
     )
 }
--- pages/contact.js
+++ pages/contact.js
@@ -5,21 +5,21 @@ export default function contact(){
     return (
         <div className="flex flex-col gap-[100px]">
             <Header />
-            <div>
-            <div className="w-[628px] mx-auto">
-                <div>Contact Us</div>
-                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</div>
+            <div className="mx-8">
+            <div className="max-w-[628px] mx-auto gap-5 flex flex-col">
+                <div className="font-semibold text-4xl text-black">Contact Us</div>
+                <div className="font-jakarta font-normal text-base text-[#696A75]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</div>
                 </div>
                 <div className="mt-[30px] mb-[49px]">
-                <div className="flex justify-between w-[638px] mx-auto">
-                    <div className="rounded-xl border-gray-400 border w-[294px] p-4 h-[133px]"><h2>Address</h2><div>1328 Oak Ridge Drive, Saint Louis, Missouri</div></div>
-                    <div className="rounded-xl border-gray-400 border w-[294px] p-4 h-[133px]"><h2>Contact</h2><div>313-332-8662 info@email.com</div></div>
+                <div className="p-[10px_0px] flex gap-[50px] flex-col md:flex-row justify-center items-center">
+                    <div className="rounded-xl border-gray-400 border w-[294px] p-4 h-[133px] flex flex-col gap-2.5"><h2 className="font-semibold text-[24px] text-black leading-10">Address</h2><div className="font-normal text-[18px] leading-[26px] text-[#696A75]">1328 Oak Ridge Drive, Saint Louis, Missouri</div></div>
+                    <div className="rounded-xl border-gray-400 border w-[294px] p-4 h-[133px] flex flex-col gap-2.5"><h2 className="font-semibold text-[24px] text-black leading-10">Contact</h2><div className="font-normal text-[18px] leading-[26px] text-[#696A75]">313-332-8662 <br></br> info@email.com</div></div>
                 </div>
             </div>
-            <div className="bg-gray-100 flex flex-col gap-6 rounded-[10px] w-[643px] mx-auto p-[29px_0px_44px_35px]">
+            <div className="bg-gray-100 flex flex-col gap-6 rounded-[10px] max-w-[643px] mx-auto p-[29px_0px_44px_35px] pr-[29px]">
                 <h2 className="font-bold">Leave a Message</h2>
-                <div className="flex flex-col gap-5 w-[478px]">
-                    <div className="flex gap-7">
+                <div className="flex flex-col gap-5 max-w-[478px]">
+                    <div className="flex gap-7 flex-col md:flex-row">
                         <input type="text" className="outline-none text-base-content rounded-[5px] p-[7px_27px_8px_18px] border border-gray-400" placeholder="Your Name"/>
                         <input type="text" className="outline-none text-base-content rounded-[5px] p-[7px_27px_8px_18px] border border-gray-400" placeholder="Your Email"/>
                     </div>
@@ -30,7 +30,7 @@ export default function contact(){
                         <textarea  className="p-[14px_20px_8px_18px] rounded-[5px] border border-gray-400 w-full h-[134px] bg-white outline-none resize-none" placeholder="Write a message"/>
                     </div>
                     <div>
-                        <button className="p-[10px_16px] bg-[#4B6BFB] rounded-md font-medium text-sm font-sans text-white mt-[9px]">Send Message</button>
+                        <button className="p-[10px_16px] bg-[#4B6BFB] rounded-md font-medium text-sm font-sans text-white mt-[9px] active:scale-95 duration-300 hover:bg-[#3f5ee7]">Send Message</button>
                     </div>
                 </div>
             </div></div>
