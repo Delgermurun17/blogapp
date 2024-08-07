27479d2c9d630530bb48a0097a8a4b88bf9dd185
Delgermurun <88150265+Delgermurun17@users.noreply.github.com>
2024-08-07 17:04:41 +0800

    depl

--- app/[author]/[slug]/page.js
+++ app/[author]/[slug]/page.js
@@ -33,11 +33,11 @@ export default async function Page({ params }){
             <div className="flex flex-col gap-5">
                 <div className="font-semibold text-4xl text-[#181a2a]">{article.title}</div>
                 <div className="flex gap-6">
-                    <div className="flex gap-2 font-medium text-sm text-[#696a75] items-center"><Image src={article.user.profile_image_90} width={28} height={28} className="rounded-[28px]"/>{article.user.name}</div>
+                <Link href={`https://dev.to/${article.user.username}`} target="_blank"><div className="flex gap-2 font-medium text-sm text-[#696a75] items-center cursor-pointer"><Image src={article.user.profile_image_90} width={28} height={28} className="rounded-[28px]"/>{article.user.name}</div></Link>
                     <div className="font-normal text-sm text-[#696a75] content-center">{dayjs(article.published_at).locale("mn").format("MMMM D, YYYY")}</div>
                 </div>
             </div>
-            <div><Image className="rounded-xl" src={article.social_image} width={800} height={462}/></div>
+            <div><Image className="rounded-xl cursor-pointer" src={article.social_image} width={800} height={462}/></div>
             <div className="prose">{parse(article.body_html)}</div>
         </div>
         </div>
